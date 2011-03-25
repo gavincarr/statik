ecd2ca4588a280ed4a77b799dfe2f9f41314a2f6
Gavin Carr <gavin@openfusion.com.au>
2011-03-25 22:02:16 +0000

    Add some initial pod to Statik::Plugin.

--- lib/Statik/Plugin.pm
+++ lib/Statik/Plugin.pm
@@ -49,3 +49,110 @@ sub options {
 }
 
 1;
+
+=head1 NAME
+
+Statik::Plugin - base class for statik plugins
+
+=head1 SYNOPSIS
+
+  # Example plugin
+  package Statik::Plugin::Sample;
+
+  use strict;
+  use parent qw(Statik::Plugin);
+
+  # Plugins should provide a 'defaults' sub if they wish to provide any
+  # plugin settings that can be configured in a [Statik::Plugin::Name]
+  # section in the main statik.conf.
+  sub defaults {
+    return {
+      name      => 'value',
+    };
+  }
+
+  # Plugin hook methods
+  sub start { ... }
+  sub post  { ... }
+  sub end   { ... }
+
+  1;
+
+
+=head1 DESCRIPTION
+
+Statik::Plugin is the base class for statik plugins, providing a base
+constructor and various utility methods to plugins.
+
+=head2 CONSTRUCTOR
+
+The base constructor handles setting up config settings either from the
+defaults hash returned by the plugin defaults subroutine, or from any
+plugin section in the statik.conf file (overriding the defaults). Plugin
+config variables are made available as top-level attributes in the plugin
+object i.e. if you define a config item of 'name', it will be defined and
+available as $self->{name} to the plugin.
+
+=head2 UTILITY METHODS
+
+Statik::Plugin defines a number of utility methods for plugins:
+
+=over 4
+
+=item config()
+
+Returns the top-level Statik::Config object with the settings defined
+in the main statik.conf config file.
+
+=item options()
+
+Returns a hashref containing any runtime options in force for this statik
+run. The options which may currently be defined here include 'verbose',
+'noop', and 'force'.
+
+=back
+
+=head2 HOOK METHODS
+
+Plugins can define hook methods which are called at various stages of
+processing by the main statik library and generator. The set of hooks
+which can be defined, and their expected arguments, are defined below.
+
+All hooks are passed the $self object, and then a set of name => value
+pairs as arguments, so that a typical hook subroutine might look like
+this:
+
+    sub post {
+        my ($self, %args) = @_;
+        my $stash = $args{stash};
+        # Hook implementation
+    }
+
+=over 4
+
+=item start
+
+=item head
+
+=item post
+
+=item foot
+
+=item end
+
+=back
+
+=head1 AUTHOR
+
+Gavin Carr <gavin@openfusion.com.au>
+
+=head1 COPYRIGHT AND LICENCE
+
+Copyright (C) Gavin Carr 2011.
+
+This library is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself, either Perl version 5.8.0 or, at
+your option, any later version of Perl 5.
+
+=cut
+
